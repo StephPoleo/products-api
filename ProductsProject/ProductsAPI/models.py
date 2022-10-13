9ecbf7ad4cf9b789bbc4bb997983e2f18b34a586
Stephanie <stephaniepoleo@hotmail.com>
2022-10-13 11:17:55 -0500

    Creating model

--- ProductsProject/ProductsAPI/models.py
+++ ProductsProject/ProductsAPI/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Product(models.Model):
+
+    #SKU stands for ‘Stock Keeping Unit.’ It is a unique alphanumeric code that identifies 
+    # a product to help retailers keep track of their inventory.
+    sku = models.CharField(max_length=36, default=uuid.uuid4)
+    name= models.CharField(max_length=50)
+    price = models.FloatField()
+    brand= models.CharField(max_length=50)
