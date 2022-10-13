40d637e0f7874a408c83662210f5831e50d7762d
Stephanie <stephaniepoleo@hotmail.com>
2022-10-13 18:24:51 -0500

    Creating serializer

--- ProductsProject/ProductsAPI/serializers.py
+++ ProductsProject/ProductsAPI/serializers.py
@@ -1,8 +1,25 @@
 from dataclasses import field
 from rest_framework import serializers
-from .models import Product
+from .models import Product, User
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk',
+        )
+
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ('name', 'price', 'brand', 'url')
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    url = serializers.HyperlinkedIdentityField(
+        view_name='user-detail',
+        lookup_field='pk',
+        )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'url')
